99acf72f26f45c49e7cd4cdee992d7b8268c8f6d
0xJonaseb11 <sebejaz99@gmail.com>
2024-10-14 21:34:40 +0200

    update init deploy contract

---
+++ migrations/1_deploy_pharmaModel.js
@@ -0,0 +1,16 @@
+const PharmaModel = artifacts.require("PharmaModel");
+
+module.exports = async function (deployer, network, accounts) {
+
+    const AXELAR_GATEWAY_ADDRESS = "0xe432150cce91c13a887f7D836923d5597adD8E31";
+
+    // Define the initial supply – for example, 100 tokens with 18 decimals
+  const initialSupply = web3.utils.toWei("100000000", "ether"); // Mints 100M tokens
+
+    // Deploy the BurnableToken contract with the required constructor parameters
+    await deployer.deploy(
+        PharmaModel,
+        AXELAR_GATEWAY_ADDRESS,
+        initialSupply
+    );
+};
